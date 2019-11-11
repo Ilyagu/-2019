8c69e55686b6ff1fd1c88561fe0ec1a266806685
Ильягу <55689696+Ilyagu@users.noreply.github.com>
2019-11-11 05:03:41 +0300

    Lab.2 v.1

---
+++ BKIT 3 semestr/Lab.2/main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Laba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Нагдимаев Ильягу ИУ5-35Б";
+            // object rectangle 24x42
+            Rectangle a = new Rectangle(57, 12);
+            // object quadrate 56x56
+            Quadrate b = new Quadrate(76);
+            // object circle r=77
+            Circle c = new Circle(40);
+
+            a.Print();
+            Console.WriteLine();
+
+            b.Print();
+            Console.WriteLine();
+
+            c.Print();
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
